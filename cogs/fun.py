307431d57a87cd3e2a91c9a2ee449a6175f1fa88
Paws <lyric_wolf@outlook.com>
2018-10-15 19:55:38 +0100

    lul

--- cogs/fun.py
+++ cogs/fun.py
@@ -16,5 +16,13 @@ class Fun_Commands:
         t_echo = text.replace("@", "@\u200B").replace("&", "&\u200B")
         await ctx.send(f"{t_echo}")
 
+    @commands.command(hidden=True)
+    async def cinder(self, ctx, msg):
+        """
+        xd
+        """
+        await ctx.message.delete()
+        await ctx.send("Cinder is gay. That's why he's called daddy.")
+
 def setup(bot):
     bot.add_cog(Fun_Commands(bot))
