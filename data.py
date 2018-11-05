4212ee742e45a180dec243904ab21a0b03b380c5
Paws <lyric_wolf@outlook.com>
2018-11-05 17:41:30 +0000

    There's your issue

--- data.py
+++ data.py
@@ -15,7 +15,7 @@ class Bot(AutoShardedBot):
         if not self.is_ready() or msg.author.bot or not permissions.can_send(msg) or msg.guild is None:
             return
         else:
-            if (msg.channel.id == 423890150876250122 or msg.channel.id == 444787343593832448) and (msg.attachments or "http" in msg.content) and not msg.author.bot:
+            if (msg.channel.id == 445658065933434892 or msg.channel.id == 445659536016277514) and (msg.attachments or "http" in msg.content) and not msg.author.bot:
                 upvote = self.get_emoji(507362047059689472)
                 await msg.add_reaction(upvote)
                 await self.process_commands(msg)
