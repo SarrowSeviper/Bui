4a052341ab2ba78e4297a77fbd3c54ddfabfe570
Paws <lyric_wolf@outlook.com>
2018-11-09 18:44:10 +0000

    intiger wai

--- cogs/admin.py
+++ cogs/admin.py
@@ -167,7 +167,7 @@ class Admin:
     @commands.command()
     @commands.guild_only()
     @commands.check(repo.is_owner)
-    async def setupvotes(self, ctx, member: discord.Member, votestoset):
+    async def setupvotes(self, ctx, member: discord.Member, votestoset: int = 0):
         """Does what it says on the tin"""
         query = "SELECT * FROM artstats WHERE userid=$1"
         row = await self.bot.db.fetchrow(query, member.id)
