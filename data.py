42d80d2c2ce5c98af1c62a009ef3d1992da80c07
Paws <lyric_wolf@outlook.com>
2018-12-03 20:20:41 +0000

    Fixed bug

--- data.py
+++ data.py
@@ -16,10 +16,9 @@ class Bot(AutoShardedBot):
         
         if not self.is_ready() or msg.author.bot or not permissions.can_send(msg) or msg.guild is None or msg.author.id in blacklist:
             return
+        if (msg.channel.id == 445658065933434892 or msg.channel.id == 510141164179947574 or msg.channel.id == 445659536016277514) and (msg.attachments or "http" in msg.content) and not msg.author.bot:
+            upvote = self.get_emoji(507362047059689472)
+            await msg.add_reaction(upvote)
+            await self.process_commands(msg)
         else:
-            if (msg.channel.id is 445658065933434892 or msg.channel.id is 445659536016277514 or msg.channel.id is 510141164179947574) and (msg.attachments or "http" in msg.content) and not msg.author.bot:
-                upvote = self.get_emoji(507362047059689472)
-                await msg.add_reaction(upvote)
-                await self.process_commands(msg)
-            else:
-                await self.process_commands(msg)
+            await self.process_commands(msg)
