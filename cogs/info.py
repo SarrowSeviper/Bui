03641eceb8802e80aee680224a694fd01fb4da31
Paws <lyric_wolf@outlook.com>
2019-02-01 20:40:01 +0000

    Shh

--- cogs/info.py
+++ cogs/info.py
@@ -211,37 +211,37 @@ class Information:
             )
         await ctx.send(embed=embed)
 
-    # @commands.command()
-    # async def secretsanta(self, ctx):
-    #     """ Registers for the Secret Santa! """
+    @commands.command()
+    async def valentines(self, ctx):
+        """ Registers for the Valentines Day! """
 
-    #     def check(reaction, user):
-    #         return user == ctx.author and str(reaction.emoji) == "🎟"
+        def check(reaction, user):
+            return user == ctx.author and str(reaction.emoji) == "🎟"
 
-    #     await ctx.message.delete()
-    #     msg = await ctx.author.send(
-    #         f"**{ctx.author.name}**, by entering, you are committed to drawing whatever you've been presented with. React to confirm your entry into the event."
-    #     )
-    #     await msg.add_reaction("🎟")
-    #     try:
-    #         await self.bot.wait_for("reaction_add", timeout=60.0, check=check)
-    #     except asyncio.TimeoutError:
-    #         await ctx.author.send("Timed out..")
-    #         await msg.delete()
-    #     else:
-    #         dmmsg = await ctx.author.send("You have been entered into the Secret Santa")
-    #         await msg.delete()
-    #         hook = Webhook(self.config.santahook, is_async=True)
-    #         embed = Embed(
-    #             title="Someone entered the event!",
-    #             description=f"User: {ctx.author.mention}\nTag: {ctx.author.name}#{ctx.author.discriminator}\nID: {ctx.author.id}",
-    #             color=0xDB1F1F,
-    #             timestamp=True,
-    #         )
-    #         await hook.send(embeds=embed)
-    #         await hook.close()
-    #         await asyncio.sleep(5)
-    #         await dmmsg.delete()
+        await ctx.message.delete()
+        msg = await ctx.author.send(
+            f"**{ctx.author.name}**, by entering, you are committed to drawing whatever you've been presented with. React to confirm your entry into the event."
+        )
+        await msg.add_reaction("🎟")
+        try:
+            await self.bot.wait_for("reaction_add", timeout=60.0, check=check)
+        except asyncio.TimeoutError:
+            await ctx.author.send("Timed out..")
+            await msg.delete()
+        else:
+            dmmsg = await ctx.author.send("You have been entered into the Valentines Day Event!")
+            await msg.delete()
+            hook = Webhook(self.config.santahook, is_async=True)
+            embed = Embed(
+                title="Someone entered the event!",
+                description=f"User: {ctx.author.mention}\nTag: {ctx.author.name}#{ctx.author.discriminator}\nID: {ctx.author.id}",
+                color=0xDB1F1F,
+                timestamp=True,
+            )
+            await hook.send(embeds=embed)
+            await hook.close()
+            await asyncio.sleep(5)
+            await dmmsg.delete()
 
 
 def setup(bot):
